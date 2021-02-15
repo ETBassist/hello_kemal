5e3cf86004518eae3017547c204d88cf72dfa64c
Eugene Theriault <hybridbassist@gmail.com>
2021-02-14 17:28:05 -0800

    Create working hello world root path

---
+++ src/hello_kemal.cr
@@ -0,0 +1,15 @@
+require "kemal"
+
+get "/" do
+  "Hello World!"
+end
+
+
+# TODO: Write documentation for `HelloKemal`
+module HelloKemal
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
+
+Kemal.run
